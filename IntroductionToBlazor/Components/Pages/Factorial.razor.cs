35f67befe149e4a0ecc911f4e6ad6db35400cc32
hawri <hawri@DESKTOP-BMM1386>
2025-09-18 13:00:39 +0300

    TODOlist in progress

---
+++ IntroductionToBlazor/Components/Pages/Factorial.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace IntroductionToBlazor.Components.Pages
+{
+    public partial class Factorial
+    {
+        int n = 0;
+        long factorial = 1;
+        void Calculate()
+        {
+            factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+        }
+    }
+}
