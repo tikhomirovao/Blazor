78c0b54fde1d7fda72835f8b64281e16a23e0418
hawri <hawri@DESKTOP-BMM1386>
2025-09-23 16:06:27 +0300

    Movies DONE

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -6,10 +6,17 @@ namespace Movies.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string? Title { get; set; }
+        [RangeAttribute(typeof(DateOnly), "14-10-1888", "31-12-2032")]
         public DateOnly ReleaseDate { get; set; }
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z()\S-]*$")]
         public string? Genre { get; set; }
 
+        [Range(0, 200)]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
