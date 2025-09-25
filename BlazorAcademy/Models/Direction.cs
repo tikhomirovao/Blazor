fea2bed4b50456f0c12d27a4c1df06337d9f5f4d
hawri <hawri@DESKTOP-BMM1386>
2025-09-25 17:38:13 +0300

    BlazorAcademy in progress

---
+++ BlazorAcademy/Models/Direction.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorAcademy.Models
+{
+    public class Direction
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public byte direction_id { get; set; }
+        [Required]
+        public string direction_name { get; set; }
+    }
+}
